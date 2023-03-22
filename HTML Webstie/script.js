112ce8ad6a044bda838cb01fa5a4e44117f42434
Polarice3 <zarularief04@gmail.com>
2023-03-22 19:19:07 +1100

    Added HTML Website Example

---
+++ HTML Webstie/script.js
@@ -0,0 +1,28 @@
+function openRecipe(evt, recipeName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(recipeName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("top").style.display = "block";
+  } else {
+    document.getElementById("top").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
